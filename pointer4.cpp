833cf2ee94dfbbe83432ead6ba463609ebd061cb
kamalyadav1 <ky0592@srmist.edu.in>
2026-02-06 16:27:09 +0530

    converting data type using pointer

---
+++ pointer4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float num=45.33;
+    cout << "num = " << num << endl;
+    int n=(int)num;
+    cout << "n = " << n << endl;
+    return 0;
+}
