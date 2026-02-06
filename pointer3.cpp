da95965a12aa31cf0c0631f3b0e56ede2df54540
kamalyadav1 <ky0592@srmist.edu.in>
2026-02-06 11:23:52 +0530

    calculation of area of circle using pointer

---
+++ pointer3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int radius;
+    int area;
+    cout << "Enter radius: ";
+    cin >> radius;
+    area = 3.14*radius * radius;
+    cout << "Area = " << area << endl;
+    return 0;
+
+}
