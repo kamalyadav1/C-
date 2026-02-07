db03ce020ea1715fd887c71ebf4b044926d47483
kamalyadav1 <ky0592@srmist.edu.in>
2026-02-08 00:30:53 +0530

    finding Ascii value of the character

---
+++ Ascii.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    cout << "Enter a character: ";
+    cin >> c;
+
+
+    cout << "The ASCII value of " << c << " is " << int(c) << endl;
+
+    return 0;
+}
