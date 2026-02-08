942a15e551aeb34649398918ce1d6525653bb570
kamalyadav1 <ky0592@srmist.edu.in>
2026-02-08 22:31:00 +0530

    check either your are eligiable for voting or not

---
+++ voting.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int age;
+    cout<<"enter your age"<<endl;
+    cin>>age;
+    if (age>=18)
+    {
+        cout<<"your are aliagiable for voting ";
+    }
+    else
+    {
+        cout<<"you are not aliagiable for voting";
+    }
+    return 0;
+}
