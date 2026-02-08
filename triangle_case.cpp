a641c34a78127c6013123aab4979ae70e0fd9863
kamalyadav1 <ky0592@srmist.edu.in>
2026-02-08 23:02:47 +0530

    checking either with all this sides can make the triangle or not

---
+++ triangle_case.cpp
@@ -0,0 +1,33 @@
+checking the eithe the triangle is possible with this three sides or not
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter the lengths of the sides of the triangle"<<endl;
+    cin>>a>>b>>c;
+    if (a+b>c || b+c>a || c+a>b)
+    {
+        cout<<"the triangle is possible with this all sides";
+    }
+    else
+    {
+        cout<<"the triangle is not possible with these all please change the length of the side of the triangle";
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
