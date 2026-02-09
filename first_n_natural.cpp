468bc7d5ecad65fd123c0ad5a94247c693b49a62
kamalyadav1 <ky0592@srmist.edu.in>
2026-02-09 07:29:16 +0530

    N first natural number using for loop

---
+++ first_n_natural.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"enter any limit to the first n atural number  :"<<endl;
+    cin>>n;
+    cout<<"natural number are"<<endl;
+    for (int i=1; i<=n; i++) {
+        cout<<i<<endl;
+    }
+    return 0;
+}
