32d83602ed3160e041d2e9638af3c94943f72a64
kamalyadav1 <ky0592@srmist.edu.in>
2026-02-09 23:58:16 +0530

    sorting of the array in the Ascending order using pre defined sort function

---
+++ sort_of_Array_in_Ascending.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n[]={1,2,3,4,5};
+    cout<<"Enter a number: ";
+    int a= sizeof(n);
+    for (int i=0;i<5;i++) {
+   cin>>n[i];
+    }
+    sort(n,n+5);
+    for (int i=0;i<5;i++) {
+        cout<<n[i]<<" ";
+    }
+
+return 0;
+}
