63232f2b03965af675934f139687b63bc9a26bec
kamalyadav1 <ky0592@srmist.edu.in>
2026-02-09 22:37:37 +0530

    read and display array elements using for loop

---
+++ read_and_display_array_elements.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n[5];
+    cout<<"enter the element of the arrays"<<endl;
+    for(int i=0;i<5;i++) {
+        cin>>n[i];
+    }
+    for(int i=0;i<5;i++) {
+        cout<<n[i]<<" ";
+    }
+    return 0;
+
+}
